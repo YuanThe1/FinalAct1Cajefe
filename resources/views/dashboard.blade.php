a9c8192fe022ef87ce85ea782710ee75eee6ed50
YuanThe1 <117751835+YuanThe1@users.noreply.github.com>
2023-11-29 17:53:57 +0800

    nice

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,37 +10,32 @@
 
 
             <div class="container">
-            <div class="ro"><table class="table">
+            <div class="ro">
+<table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Id</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Created at</th>
     </tr>
   </thead>
   <tbody>
+@php
+    $i = 1;
+@endphp
+  @foreach ($users as $user)
+
+
     <tr>
       <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td>{{$user->name}}</td>
+      <td>{{$user->email}}</td>
+      <td>{{$user->created_at->diffforhumans()}}</td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    @endforeach
   </tbody>
-</table></div>    
-            </div>
-
-        </div>
-    </div>
+</table>
+</div>
+</div>
 </x-app-layout>
